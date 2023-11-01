51c2f8b9250a1ac612a5648160cc6132d9906218
Eric A. Rodríguez S <eazicomservicios@gmail.com>
2023-11-01 09:37:33 -0600

    Desarrollo del curso.
    Se crea la sección 6, una calculadora en HTML y JavaScript.

---
+++ src/section-06/calculator.ts
@@ -0,0 +1,16 @@
+/**
+ * Copyright (c) 2023 Eazicom Servicios Profesionales
+ * Derechos intelecutales reservados.
+ */
+
+/**
+ * Contiene los dígitos y operadores que deben 
+ * @type {string[]}
+ */
+const operators: string[] = ['/', 'X', '+', '-', '='];
+
+
+// Este evento de dispara al finalizar la carga completa del documento HTML.
+document.addEventListener( 'DOMContentLoaded', ( ev: Event ): void => {
+
+} );
