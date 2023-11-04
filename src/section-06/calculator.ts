fc1f39771bf382a047857e6fca78ecb128e51148
Eric A. Rodríguez S <74212894+Eazicom@users.noreply.github.com>
2023-11-03 20:06:21 -0600

    Desarrolllo del curso.
    Desarrollo de la calculadora, funcionalidad con JavaScript.

--- src/section-06/calculator.ts
+++ src/section-06/calculator.ts
@@ -10,17 +10,19 @@ type Key = JQuery<HTMLButtonElement>;
 
 type Container = JQuery<HTMLDivElement>;
 
-type ResolvedKey = ( value: JQuery<HTMLDivElement> |
-    PromiseLike<JQuery<HTMLDivElement>> ) => void;
+type Component = JQuery<HTMLDivElement>;
+
+type ResolvedComponent = ( value: Component |
+    PromiseLike<Component> ) => void;
 
 /**
- * Contiene los dígitos y operadores que deben 
+ * Contiene los dígitos y operadores que deben
  * @type {string[]}
  */
 const keys: string[] = ['CE', 'DEL', '%', '/', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '+/-', '0', '.', '='];
 
 const asDigit = ( button: Key ): void => {
-    button.addClass( 'btn-primary' );
+    button.addClass( 'btn-info' );
     button.data( 'role', 'digit' );
 }
 
@@ -32,16 +34,16 @@ const asCommand = ( button: Key ): void => {
     if ( '=' == symbol )
         className += 'danger';
     else
-        className += 'success';
+        className += 'secondary';
 
     button.addClass( className );
     button.data( 'role', 'command' );
 }
 
 /**
- * 
- * @param symbol 
- * @returns 
+ *
+ * @param symbol
+ * @returns
  */
 const createKey = ( symbol: string ): Key => {
 
@@ -57,26 +59,69 @@ const createKey = ( symbol: string ): Key => {
     return key;
 }
 
-const buildKeyboard = async ( keys: Array<string> ): Promise<void> => {
-    await ( new Promise<void>( ( resolve ) => {
-        let currentColumn = 0;
-        let row: JQuery<HTMLDivElement>;
+function buildKeyboard( keys: string[] ): Component {
+    let currentColumn = 0;
+    let row: JQuery<HTMLDivElement>;
 
-        keys.forEach( ( symbol: string ) => {
-            if ( currentColumn === 0 )
-                row = $( '<div>', { class: 'row mt-3' } );
+    let keyboard: Component = $( "<div>", { "class": "card-body" } );
 
-            let key = createKey( symbol );
-            row.append( $( '<div>', { class: 'col-3 d-grid' } ).append( key ) );
-            currentColumn++;
+    keys.forEach( ( symbol: string ) => {
+        if ( currentColumn === 0 )
+            row = $( '<div>', { class: 'row mt-3' } );
 
-            if ( currentColumn === 4 ) {
-                $( '#keyboard' ).append( row );
-                currentColumn = 0;
-            }
-        } );
-        resolve();
-    } ) );
+        let key = createKey( symbol );
+        row.append( $( '<div>', { class: 'col-3 d-grid' } ).append( key ) );
+        currentColumn++;
+
+        if ( currentColumn === 4 ) {
+            keyboard.append( row );
+            currentColumn = 0;
+        }
+    } );
+
+    return keyboard;
+}
+
+/**
+ *
+ * @param keys
+ * @returns
+ */
+async function buildCalculator( keys: string[] ): Promise<Component> {
+    return ( new Promise<Component>(
+        ( resolve: ResolvedComponent ) => {
+
+            let calculator: Component = $( "<div>", { "class": "card" } );
+
+            const screen: Component = buildScreen();
+            calculator.append( screen );
+
+            const keyboard: Component = buildKeyboard( keys );
+            calculator.append( keyboard );
+
+            resolve( calculator );
+        }
+    ) );
+}
+
+/**
+ *
+ * @param keys
+ */
+function buildScreen(): Component {
+
+    let led = $( '<input>', { type: 'text', id: 'result' } );
+    led.addClass( 'ea-result form-control fs-1 text-end' )
+        .attr( 'readonly', 'true' ).val( '0' );
+
+    let screen: Component = $( "<div>", {
+        "class": "card-header container"
+    } );
+    screen.append( $( "<div>", { "class": "row mt-2 mb-2" } )
+        .append( $( "<div>", { "class": "col" } )
+            .append( led ) ) );
+
+    return screen;
 }
 
 const configure = async () => {
@@ -89,5 +134,4 @@ const configure = async () => {
 
 // Este evento de dispara al finalizar la carga completa del documento HTML.
 document.addEventListener( 'DOMContentLoaded', ( ev: Event ): void => {
-    buildKeyboard( keys ).then();
 } );
